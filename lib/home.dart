86388e086c1fc9e8868440345703c82b86da12d9
ym1522 <ym1522@naver.com>
2022-12-14 11:15:53 +0900

    .

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last, deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'package:kiosk/orderType.dart';
 import 'camera.dart';
 
 class MyHomePage extends StatelessWidget {
