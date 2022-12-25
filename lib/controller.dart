cf4b978270cc60761d4294d6733d9bfaf51b0089
ym1522 <ym1522@naver.com>
2022-12-25 15:18:20 +0900

    221225

--- lib/controller.dart
+++ lib/controller.dart
@@ -2,11 +2,6 @@ import 'package:get/get.dart';
 
 class CountController extends GetxController {
   final quantity = 0.obs;
-  var total = 0;
-
-  void addTotal(int t){
-    total += t;
-  }
 
   void add() {
     quantity.value++;
